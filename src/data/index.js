2d9f024dd800d11cf4fe4dc19a57620d39dca2cd
Raphael Kottakal <raphaelsemail@gmail.com>
2016-05-18 14:25:15 +0530

    change SRC to Radium

--- src/data/index.js
+++ src/data/index.js
@@ -151,17 +151,17 @@ export default {
 		'Minimalist': {
 			image: 'http://assets.myntassets.com/v1463481042/reactive/gif/celeb-makeup-answer1.jpg',
 			text: 'Congratulations! You believe in ‘less is more’ when it comes to make-up; you are the minimalist. A basic foundation and a little gloss on your lips is all you need most of the time. In fact, you aren’t afraid to embrace the no make-up look for brunches or casual day outs. We do know you get a kick from all the comments that pour in on the rare occasions when you decide to add a little drama to your look by doing a bold lip or sporting winged eyes. Nudes, peach and red are your go-to lip colour.',
-			link: 'http://www.myntra.com/minimalist-natural-and-simple?SRC=Lookgood6'
+			link: 'http://www.myntra.com/minimalist-natural-and-simple?SRC=Radium'
 		},
 		'Classic': {
 			image: 'http://assets.myntassets.com/v1463481043/reactive/gif/celeb-makeup-answer2.jpg',
 			text: 'Congratulations! The ever graceful classic, for you balance is everything. You know just how to use make-up to enhance your look without ever overdoing it. Keeping it simple and tasteful is your mantra, and it works! Subtle eyeshadows, blushers, and classic lipsticks take a lot of space in your make-up kit. Occasionally you do like to try some classy make-up trends once in a while and we won’t be surprised to see you do smokey eyes. Interestingly, you make a bold feminist statement when you go for shades of pink for your power dressing.',
-			link: 'http://www.myntra.com/classic-soft-colours-and-lined-eyes?SRC=Lookgood6'
+			link: 'http://www.myntra.com/classic-soft-colours-and-lined-eyes?SRC=Radium'
 		},
 		'Daring': {
 			image: 'http://assets.myntassets.com/v1463481044/reactive/gif/celeb-makeup-answer3.jpg',
 			text: 'Congratulations! You have a bold and fun personality and this applies to your make-up style too! The one to take make-up risks and experiment with all the fun runway trends! To you make-up is a form of art which helps to unleash the theatricality of life. Black smokey eyes are your favourite and why not when they enhance your expressive eyes the way they do. Electric blue is your current favourite trend and you know all about contouring! Trust us when we tell you that girls so want to channel your cool confidence!',
-			link: 'http://www.myntra.com/flamboyant-makeup-junkie?SRC=Lookgood6'
+			link: 'http://www.myntra.com/flamboyant-makeup-junkie?SRC=Radium'
 		},
 	}
 
