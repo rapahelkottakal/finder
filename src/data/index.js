b51af17df8a7f90e383aaf0dd8649a341a8a46aa
13308 <varun.bondada@gmail.com>
2017-04-07 20:46:54 +0530

    first push for heels minigig

--- src/data/index.js
+++ src/data/index.js
@@ -8,23 +8,29 @@ export default {
 	qNa: [
 		{
 			question: {
-				text: 'http://assets.myntassets.com/v1489042469/reactive/finder/80-trend/questions/01/80s-Trend-Quiz_Question_01.jpg',
+				text: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576704845-16_01.jpg',
 				weight: 1
 			},
 			options: [
 				{
-					key: 'neons',
-					image: 'http://assets.myntassets.com/v1489042469/reactive/finder/80-trend/questions/01/80s-Trend-Quiz_Question_02.jpg',
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576704826-16_02.jpg',
 					// text: 'Shift dress and thigh-high gladiators'
 				},
 				{
-					key: 'power_shoulders',
-					image: 'http://assets.myntassets.com/v1489042470/reactive/finder/80-trend/questions/01/80s-Trend-Quiz_Question_03.jpg',
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576704806-16_03.jpg',
 					// text: 'Sundress and sneakers'
 				},
 				{
-					key: 'shimmer_sequins',
-					image: 'http://assets.myntassets.com/v1489042469/reactive/finder/80-trend/questions/01/80s-Trend-Quiz_Question_04.jpg',
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576704781-16_04.jpg',
+					// text: 'Midi skirts with crop tops'
+
+				},
+				{
+					key: 'ans',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576704758-16_05.jpg',
 					// text: 'Midi skirts with crop tops'
 
 				}
@@ -33,50 +39,61 @@ export default {
 
 		{
 			question: {
-				text: 'http://assets.myntassets.com/v1489042569/reactive/finder/80-trend/questions/02/80s-Trend-Quiz_Question_01.jpg',
+				text: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576799008-dress-finder-15_01.jpg',
 				weight: 1
 			},
 			options: [
 				{
-					key: 'neons',
-					image: 'http://assets.myntassets.com/v1489132021/reactive/finder/80-trend/questions/02/80s-Trend-Quiz_Question_02.jpg',
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576798991-dress-finder-15_02.jpg',
 					//text: 'Shift dress and thigh-high gladiators'
 				},
 				{
-					key: 'power_shoulders',
-					image: 'http://assets.myntassets.com/v1489042570/reactive/finder/80-trend/questions/02/80s-Trend-Quiz_Question_03.jpg',
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576798971-dress-finder-15_03.jpg',
 					//text: 'Sundress and sneakers'
 				},
 				{
-					key: 'shimmer_sequins',
-					image: 'http://assets.myntassets.com/v1489042570/reactive/finder/80-trend/questions/02/80s-Trend-Quiz_Question_04.jpg',
+					key: 'ans',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576798941-dress-finder-15_04.jpg',
 					//text: 'Midi skirts with crop tops'
 
-				}
+				},
+				{
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576798911-dress-finder-15_05.jpg',
+					//text: 'Sundress and sneakers'
+				},
 			]
 		},
 
 		{
 			question: {
-				text: 'http://assets.myntassets.com/v1489042649/reactive/finder/80-trend/questions/03/80s-Trend-Quiz_Question_01.jpg',
+				text: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576875667-dress-finder-14_01.jpg',
 				weight: 1
 			},
 			options: [
 				{
-					key: 'neons',
-					image: 'http://assets.myntassets.com/v1489042652/reactive/finder/80-trend/questions/03/80s-Trend-Quiz_Question_02.jpg',
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576875647-dress-finder-14_02.jpg',
 					//text: 'High heels'
 
 				},
 				{
-					key: 'power_shoulders',
-					image: 'http://assets.myntassets.com/v1489042650/reactive/finder/80-trend/questions/03/80s-Trend-Quiz_Question_03.jpg',
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576875638-dress-finder-14_03.jpg',
 					//text: 'Scarves'
 
 				},
 				{
-					key: 'shimmer_sequins',
-					image: 'http://assets.myntassets.com/v1489042650/reactive/finder/80-trend/questions/03/80s-Trend-Quiz_Question_04.jpg',
+					key: 'ans',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576875624-dress-finder-14_04.jpg',
+					//text: 'Sling bags'
+
+				},
+				{
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576875611-dress-finder-14_05.jpg',
 					//text: 'Sling bags'
 
 				}
@@ -86,75 +103,52 @@ export default {
 
 		{
 			question: {
-				text: "http://assets.myntassets.com/v1489042732/reactive/finder/80-trend/questions/04/80s-Trend-Quiz_Question_01_01.jpg",
+				text: "http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576963798-dress-finder-13_01.jpg",
 				weight: 1
 			},
 			options: [
 				{
-					key: 'neons',
-					image: 'http://assets.myntassets.com/v1489042732/reactive/finder/80-trend/questions/04/80s-Trend-Quiz_Question_01_02.jpg',
+					key: 'ans',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576963781-dress-finder-13_02.jpg',
 					//text: 'Bold checks'
 
 				},
 				{
-					key: 'power_shoulders',
-					image: 'http://assets.myntassets.com/v1489042732/reactive/finder/80-trend/questions/04/80s-Trend-Quiz_Question_01_03.jpg',
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576963766-dress-finder-13_03.jpg',
 					//text: 'Jail stripes'
 				},
 				{
-					key: 'shimmer_sequins',
-					image: 'http://assets.myntassets.com/v1489042732/reactive/finder/80-trend/questions/04/80s-Trend-Quiz_Question_01_04.jpg',
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576963749-dress-finder-13_04.jpg',
+					//text: 'Leopard print'
+				},
+				{
+					key: 'wrongAns',
+					image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491576963731-dress-finder-13_05.jpg',
 					//text: 'Leopard print'
 				}
 
 			]
 		},
 
-		{
-			question: {
-				text: "http://assets.myntassets.com/v1489132116/reactive/finder/80-trend/questions/05/80s-Trend-Quiz_Question_01.jpg",
-				weight: 1
-			},
-			options: [
-				{
-					key: 'neons',
-					image: 'http://assets.myntassets.com/v1489132117/reactive/finder/80-trend/questions/05/80s-Trend-Quiz_Question_02.jpg',
-					//text: 'Strawberry crepes'
-				},
-				{
-					key: 'power_shoulders',
-					image: 'http://assets.myntassets.com/v1489132117/reactive/finder/80-trend/questions/05/80s-Trend-Quiz_Question_03.jpg',
-					//text: 'Pepperoni pizza'
-				},
-				{
-					key: 'shimmer_sequins',
-					image: 'http://assets.myntassets.com/v1489132117/reactive/finder/80-trend/questions/05/80s-Trend-Quiz_Question_04.jpg',
-					//text: 'Fish and chips'
-				}
-			]
-		},
-
 	],
 
+
 	results: {		
-		'neons': {
-			image: 'http://assets.myntassets.com/v1489044061/reactive/finder/80-trend/results/80s-Trend-Quiz_Answer_1.jpg',
-			text: 'You’re peppy, cool and gravitate towards the eccentric. You like the glint of tin over the glint of gold. Your carefree and crazy spirit deserves some equally crazy clothing – the neons from the 80s will fit that bill perfectly. Shop some now!',
-			menLink: 'http://www.myntra.com/vibrant-in-neon-clothes',
+		'ans': {
+			image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491577339753-dress-finderdf.jpg',
+			text: '',
+			menLink: '',
 			
 		},
-		'power_shoulders': {
-			image: 'http://assets.myntassets.com/v1489044054/reactive/finder/80-trend/results/80s-Trend-Quiz_Answer_2.jpg',
-			text: 'You can’t stand a hair out of place and align everything the way you want. You demand substance just as much as style, and the go-getter in you will love the commanding aura that power dressing exudes. Try it here.',
-			menLink: 'http://www.myntra.com/sharp-and-smart',
+		'wrongAns': {
+			image: 'http://assets.myntassets.com/assets/images/lookbook/2017/4/7/11491577339764-dress-finder.jpg',
+			text: '',
+			menLink: '',
 			
 		},
-		'shimmer_sequins': {
-			image: 'http://assets.myntassets.com/v1489044055/reactive/finder/80-trend/results/80s-Trend-Quiz_Answer_3.jpg',
-			text: 'imple and sober are the last adjectives on your mind.You’ve devoted your life to sparkle and shine, hence will adore the sequin-ny and shimmery trend brought by the revival of 80s. Steal the spotlight with these.',
-			menLink: 'http://www.myntra.com/glitzy-and-glamorous',
-			
-		}
+		
 	}
 
 }
